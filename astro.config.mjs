51dd1ce56718928eea240de6b69c7dbb2f97dbf8
mahesh-143 <maheshodedara13@yahoo.com>
2024-03-29 22:04:49 +0530

    update sitemap url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://maheshodedara.netlify.app",
+  site: "https://itsmahesh.xyz",
   integrations: [icon(), mdx(), sitemap()],
 });
