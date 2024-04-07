ff075f7297eab7386af0eb3979af9791207aaf68
mahesh-143 <maheshodedara13@yahoo.com>
2024-04-07 18:11:51 +0530

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://itsmahesh.xyz",
   integrations: [icon(), mdx(), sitemap()],
-  // vercel
+  //vercel
   output: "server",
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
 });
+
