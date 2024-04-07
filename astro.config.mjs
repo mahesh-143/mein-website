cf1741d4425a1635d14ff745ab400f76fbf8419f
mahesh-143 <maheshodedara13@yahoo.com>
2024-04-07 18:03:30 +0530

    Add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://itsmahesh.xyz",
   integrations: [icon(), mdx(), sitemap()],
+  // vercel
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
